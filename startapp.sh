0920e85676db9dd78f8aa39ac2c5e33e0f92707c
rheiland <randy.heiland@gmail.com>
2025-01-23 14:28:19 -0500

    init

--- startapp.sh
+++ startapp.sh
@@ -1,6 +1,3 @@
 #!/bin/sh -f
 
-# The path `/opt/qupath/infile.tiff` is important as the Galaxy IT is inserting the user-chosen file into this path.
-
-HOME="/tmp"
-_JAVA_OPTIONS="-Duser.home=${HOME} -Djavafx.cachedir=/tmp" exec /opt/qupath/QuPath/bin/QuPath --image /opt/qupath/infile.tiff --quiet
+/usr/local/pcstudio-venv/bin/python3 /opt/pcstudio/bin/studio.py -c /opt/pcstudio/config/PhysiCell_settings.xml
